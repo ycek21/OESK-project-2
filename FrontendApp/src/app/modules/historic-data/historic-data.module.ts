d9b3d2ea6a6d02977b22459a4681267d65ef4e2c
Jacek Eichler <jacekeichler@gmail.com>
2022-01-14 15:17:12 +0100

    Historic data

---
+++ FrontendApp/src/app/modules/historic-data/historic-data.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HistoricDataComponent } from './historic-data.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HistoricDataComponent]
+})
+export class HistoricDataModule { }
